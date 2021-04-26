c0330b0c44a3ada07fcf4df3464aa65bc8a16de5
Yuri Miranda Fialho <yuri@crmpiperun.com>
2021-04-26 00:04:46 -0300

    MÓDULOS: 1) Fundamentos 2) Estruturas de Controle

---
+++ controles/desafioswitch/desafioswitch.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func notaParaConceito(n float64) string {
+	var nota = int(n)
+
+	switch {
+	case nota >= 9 && nota <= 10:
+		return "A"
+	case nota >= 8 && nota < 9:
+		return "B"
+	case nota >= 5 && nota < 8:
+		return "C"
+	case nota >= 3 && nota < 5:
+		return "D"
+	default:
+		return "E"
+	}
+
+}
+
+func main() {
+	fmt.Println(notaParaConceito(9.8))
+	fmt.Println(notaParaConceito(6.9))
+	fmt.Println(notaParaConceito(2.1))
+}
